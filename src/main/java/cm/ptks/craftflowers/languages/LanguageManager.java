ddeea30de427b275c7810c5cd3ad1a1ec6f2a6d5
David Mayr <davidliebtkekse@gmail.com>
2022-10-07 07:01:49 +0000

    Finished language loading

--- src/main/java/cm/ptks/craftflowers/languages/LanguageManager.java
+++ src/main/java/cm/ptks/craftflowers/languages/LanguageManager.java
@@ -1,7 +1,15 @@
 package cm.ptks.craftflowers.languages;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+
+import com.fastasyncworldedit.core.configuration.file.YamlConfiguration;
 
 import cm.ptks.craftflowers.CraftFlowers;
 
@@ -14,6 +22,32 @@ public class LanguageManager {
         this.plugin = plugin;
     
         for(String language : plugin.getConfig().getStringList("language.list")) {
+            File languageFile = new File(plugin.getDataFolder(), "lang/" + language + ".yml");
+
+            if(!languageFile.exists()) {
+                InputStream inputStream = plugin.getResource("lang/" + language + ".yml");
+                if(inputStream == null) {
+                    plugin.getLogger().warning("Failed to load or copy " + language + "...");
+                    continue;
+                }
+                languageFile.getParentFile().mkdirs();
+                
+                try {
+                    languageFile.createNewFile();
+                } catch (IOException e) {
+                    plugin.getLogger().log(Level.SEVERE, "Failed to create language file for " + language + "...", e);
+                    continue;
+                }
+                try (FileOutputStream os = new FileOutputStream(languageFile)) {
+                    os.write(inputStream.readAllBytes());
+                    inputStream.close();
+                } catch (IOException e) {
+                    plugin.getLogger().log(Level.SEVERE, "Failed to load or copy " + language + "...", e);
+                    continue;
+                }
+            }
+            YamlConfiguration configuration = YamlConfiguration.loadConfiguration(languageFile);
+            this.languages.add(new Language(Language.parseKeys(configuration), language));
 
         }
     }
