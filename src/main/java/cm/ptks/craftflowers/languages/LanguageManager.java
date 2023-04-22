d7c9a0be40142959c5bcb3d56b440c1c4dfda1fb
David Mayr <davidliebtkekse@gmail.com>
2023-04-23 00:43:24 +0200

    fix: add new language strings with right charset

--- src/main/java/cm/ptks/craftflowers/languages/LanguageManager.java
+++ src/main/java/cm/ptks/craftflowers/languages/LanguageManager.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -58,7 +59,7 @@ public class LanguageManager {
                 if(resource != null) {
                     try {
                         YamlConfiguration configuration = YamlConfiguration.loadConfiguration(languageFile);
-                        configuration.addDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(resource)));
+                        configuration.addDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(resource, StandardCharsets.UTF_8)));
                         configuration.options().copyDefaults(true);
                         configuration.save(languageFile);
                         resource.close();
