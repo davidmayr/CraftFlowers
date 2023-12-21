0aa35a0198b3f07fdc2d3828a7c0806c01beb54e
David Mayr <davidliebtkekse@gmail.com>
2023-12-21 22:50:20 +0100

    chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "at.toastiii.craftflowers"
-version = "1.2.3"
+version = "1.2.4"
 
 repositories {
     maven(url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/"))
