f69a9ffdd66200c21d9b272f6359f700d7500556
David Mayr <davidliebtkekse@gmail.com>
2025-01-11 15:07:30 +0100

    fix: head error

--- src/main/java/cm/ptks/craftflowers/util/ItemBuilder.java
+++ src/main/java/cm/ptks/craftflowers/util/ItemBuilder.java
@@ -2,13 +2,17 @@ package cm.ptks.craftflowers.util;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URL;
 import java.util.Base64;
 import java.util.UUID;
 
@@ -32,19 +36,32 @@ public class ItemBuilder {
             return this;
 
         SkullMeta headMeta = (SkullMeta) itemStack.getItemMeta();
-        GameProfile profile = new GameProfile(UUID.randomUUID(), "null");
         byte[] encodedData = Base64.getEncoder().encode(String.format("{textures:{SKIN:{url:\"%s\"}}}",
                 "https://textures.minecraft.net/texture/" + textureId).getBytes());
-        profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
+
         try {
-            Method method = headMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
-            method.setAccessible(true);
-            method.invoke(headMeta, profile);
-            method.setAccessible(false);
-        } catch (IllegalArgumentException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e1) {
-            e1.printStackTrace();
+            PlayerProfile playerProfile = Bukkit.createPlayerProfile(UUID.randomUUID(), "null");
+            PlayerTextures textures = playerProfile.getTextures();
+            textures.setSkin(new URL("https://textures.minecraft.net/texture/" + textureId));
+            playerProfile.setTextures(textures);
+            headMeta.setOwnerProfile(playerProfile);
+        } catch (Throwable e) {
+            //Support older versions
+
+            GameProfile profile = new GameProfile(UUID.randomUUID(), "null");
+
+            profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
+            try {
+                Method method = headMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
+                method.setAccessible(true);
+                method.invoke(headMeta, profile);
+                method.setAccessible(false);
+            } catch (IllegalArgumentException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e1) {
+                e1.printStackTrace();
+            }
         }
         itemStack.setItemMeta(headMeta);
+
         return this;
     }
 
